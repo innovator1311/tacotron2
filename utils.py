4ccfa0686c246d86d354023f0ad9590d79d4fe9e
innovator1311 <tri.m.nguyen.1999@gmail.com>
2021-02-20 16:33:09 +0700

    f

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.io.wavfile import read
+import librosa
 import torch
 
 from hparams import create_hparams
@@ -16,7 +17,8 @@ def get_mask_from_lengths(lengths):
 
 
 def load_wav_to_torch(full_path):
-    sampling_rate, data = read(full_path)
+    #sampling_rate, data = read(full_path)
+    data, sampling_rate = librosa.load(full_path, sr=22050)
     return torch.FloatTensor(data.astype(np.float32)), sampling_rate
 
 
